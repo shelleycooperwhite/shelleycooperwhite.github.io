4bde3a27408145c317f9a4efef4770cbc76ef4c3
Shelley Cooper-White <hello@shelley.io>
2020-07-12 15:55:19 +1000

    Convert to typescript. Recreate "// todo: website" in gatsby. Add chakra-ui.

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { CSSReset, Heading } from "@chakra-ui/core"
+
+export default function Home() {
+  return (
+    <>
+      <CSSReset/>
+      <Heading as="h3" size="md" color="gray.500" p={2}>
+        // todo: website
+      </Heading>
+    </>
+  )
+}
